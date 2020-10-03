04cd5a20f64624e52e682bd3158899471087bfec
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-03 20:24:26 +0530

    [Akash] Added method to check attendance

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program ");
+            const int IS_PRESENT = 1;
+            int attendance = Attendance();
+            if (attendance == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            Console.ReadKey();
+        }
+        static int Attendance()
+        {
+            Random random = new Random();
+            return random.Next(0, 2);
         }
     }
 }
