8a3cee9eefd2ebcf44257e486b68e094d23b95e0
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-04 00:13:59 +0530

    [Akash] Added UC9 EmpWageBuilderObject

---
+++ Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProblem
+{
+    class EmpWageBuilderObject
+    {
+        private string company;
+        private int empRatePerHour, numOfWorkingDays, maxWorkingHrs;
+        private int totalEmpWage;
+        public EmpWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxWorkingHrs)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxWorkingHrs = maxWorkingHrs;
+            this.totalEmpWage = 0;
+        }
+        const int IS_ABSENT = 0;
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+        
+        public void CalculateEmpWage()
+        {
+            Console.WriteLine("Wage Calculation for Employee of " + this.company);
+            Random random = new Random();
+            int empHours = 0, totalWorkingHrsCumulative = 0;
+            for (int currentDay = 1; currentDay <= numOfWorkingDays && totalWorkingHrsCumulative <= maxWorkingHrs; currentDay++)
+
+            {
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_ABSENT:
+                        empHours = 0;
+                        break;
+                    case IS_PART_TIME:
+                        empHours = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHours = 8;
+                        break;
+                    default:
+                        break;
+                }
+                if (totalWorkingHrsCumulative + empHours > this.maxWorkingHrs)
+                    empHours = 0;
+                totalWorkingHrsCumulative += empHours;
+                Console.WriteLine("Day " + (currentDay) + " Employee Hours " + empHours);
+
+            }
+            Console.WriteLine("Total Employee Hours " + totalWorkingHrsCumulative);
+            totalEmpWage = totalWorkingHrsCumulative * this.empRatePerHour;
+            Console.WriteLine("Total Employee Wage " + totalEmpWage + "\n"+ "\n");
+
+        }
+        public void GetDetails()
+        {
+            Console.WriteLine("                                  ");
+            Console.WriteLine("Total Employee Wage for " + this.company + " Employee is: " + totalEmpWage);
+            Console.WriteLine("                                 \n");
+        }
+    }
+ }
+    
+
+
--- Program.cs
+++ Program.cs
@@ -2,95 +2,98 @@
 
 namespace EmployeeWageProblem
 {
+    /// <summary>
+    /// 
+    /// /</summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program ");
+            
 
-            ComputeWage("Dmart", 20, 20, 100 );
-
-            Console.WriteLine("                                             ");
-
-            ComputeWage("Reliance", 15, 25, 120);
+            EmpWageBuilderObject SapLabsWageBuilder = new EmpWageBuilderObject("Sap Labs", 200, 20, 100);
+            EmpWageBuilderObject DEShawWageBuilder = new EmpWageBuilderObject("DE Shaw", 250, 20, 70);
+            SapLabsWageBuilder.CalculateEmpWage();
+            DEShawWageBuilder.CalculateEmpWage();
+            SapLabsWageBuilder.GetDetails();
+            DEShawWageBuilder.GetDetails();
             Console.ReadKey();
         }
+        ///
+        // static void ComputeWage(string company, int wagePerHour, int numOfWorkingDays, int maxNoOfHrs)
 
-        static void ComputeWage(string company, int wagePerHour, int numOfWorkingDays, int maxNoOfHrs)
+        //  { 
 
-         { 
-            
-            const int IS_PRESENT = 1;
-            const int IS_ABSENT = 0;
-            const int IS_PART_TIME = 0;
-            const int IS_FULL_TIME = 1;
-            
-
-
-            int workingDays = 0;
-            int dailyEmployeeWage = 0;
-            int totalWage = 0;
-            int workingHrs = 0;
+        //    const int IS_PRESENT = 1;
+        //    const int IS_ABSENT = 0;
+        //    const int IS_PART_TIME = 0;
+        //    const int IS_FULL_TIME = 1;
 
 
-            Console.WriteLine("This computation is for {0}", company);
-            while(workingHrs <= maxNoOfHrs && workingDays <= numOfWorkingDays )
-            {
-                int attendance = Attendance();
-                int empCheck = EmpCheck();
-                switch (attendance)
-                {
-                    case IS_PRESENT:
-                       
-                        switch(empCheck)
-                        {
-                             
-                        case IS_FULL_TIME:
-                         Console.WriteLine("The employee is present, full time on day {0}", workingDays);
-                         dailyEmployeeWage = wagePerHour*8;
-                          workingDays++;
-                                break;
+
+        ///   int workingDays = 0;
+        //    int dailyEmployeeWage = 0;
+        //   int totalWage = 0;
+        //   int workingHrs = 0;
 
 
-                            case IS_PART_TIME:
-                        
-                            dailyEmployeeWage = wagePerHour*4;
-                            Console.WriteLine("The employee is present, part time on day {0}", workingDays);
-                            workingDays++;
-                                break;
-                            default:
-                                break;
-                        }
-                        break;
-                    case IS_ABSENT:
+        //   Console.WriteLine("This computation is for {0}", company);
+        //   while(workingHrs <= maxNoOfHrs && workingDays <= numOfWorkingDays )
+        //   {
+        //        int attendance = Attendance();
+        //       int empCheck = EmpCheck();
+        //      switch (attendance)
+        //       {
+        //          case IS_PRESENT:
+        //              
+        //                switch(empCheck)
+        //                {
+
+        //                   case IS_FULL_TIME:
+        //                  Console.WriteLine("The employee is present, full time on day {0}", workingDays);
+        //                dailyEmployeeWage = wagePerHour*8;
+        //               workingDays++;
+        //                   break;
+        //
+        //
+        ///             case IS_PART_TIME:                        
+      //  dailyEmployeeWage = wagePerHour*4;
+       //                     Console.WriteLine("The employee is present, part time on day {0}", workingDays);
+         //                   workingDays++;
+       //                         break;
+         //                   default:
+         //                       break;
+         //               }
+         //               break;
+          //          case IS_ABSENT:
                        
                        // Console.WriteLine("The employee is absent");
-                        break;
-                    default:
-                        break;
+         //               break;
+        //            default:
+       //                 break;
                 }
-
+    /// <summary>
+    /// 
+    /// </summary>
                 
-                totalWage = totalWage + dailyEmployeeWage;
+       //         totalWage = totalWage + dailyEmployeeWage;
 
-            }
-            Console.WriteLine("The total wage of employee for the month is "+ totalWage);
+      //      }
+       //     Console.WriteLine("The total wage of employee for the month is "+ totalWage);
             
             
             
             
-        }
-        static int Attendance()
-        {
-            Random random = new Random();
-            return random.Next(0, 2);
-        }
-        static int EmpCheck()
-        {
-
-            Random random = new Random();
-            return random.Next(0, 2);
-        }
-       
-    }
+    //    }
+  //      static int Attendance()
+  //      {
+  //          Random random = new Random();
+  //          return random.Next(0, 2);
+ //       }
+ //       static int EmpCheck()
+  //          Random random = new Random();
+   //         return random.Next(0, 2);
+  //      }
+ ///      
+  //  }
 }
